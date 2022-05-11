9cc8bb8a0e16751c442d31a4e0000b243492a241
dgarridoa <diego.garrido.6568@gmail.com>
2022-05-10 20:06:19 -0400

    Add unit tests to question_1.

---
+++ tests/question_1/test_question_1.py
@@ -0,0 +1,16 @@
+from cencosud.question_1.random_generator import HumanResourcesGenerator
+from cencosud.question_1.human_resources import HumanResources
+
+
+class TestHumanResources:
+    hrg = HumanResourcesGenerator()
+    hrg.sample()
+    hr = HumanResources()
+    hr.create_random_db(hrg)
+
+    def test_get_avg_children_per_marriage(self):
+        assert self.hr.get_avg_children_per_marriage() == 2.4
+
+    def test_get_person_with_max_number_grandchildren(self):
+        person_id = self.hr.get_person_with_max_number_grandchildren()["id"]
+        assert (person_id == 768)
