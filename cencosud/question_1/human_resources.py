be6038d747ebd0f8487532db174cbc2f8949d291
dgarridoa <diego.garrido.6568@gmail.com>
2022-06-03 17:18:43 -0400

    Update question_1 with sqlalchemy and pydantic.

--- cencosud/question_1/human_resources.py
+++ cencosud/question_1/human_resources.py
@@ -1,8 +1,11 @@
-import time
-import mysql.connector
+import os
+from pydantic import BaseModel, PrivateAttr, NonNegativeInt
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine.base import Engine
+from cencosud.question_1.random_generator import HumanResourcesGenerator
 
 
-class HumanResources:
+class HumanResources(BaseModel):
     """
     This class manage human resources data in a relational database.
 
@@ -13,87 +16,75 @@ class HumanResources:
     conection : MySQLConnection
         Object that open a conetion to a MySQL database.
     """
+    timeout: NonNegativeInt = 60
+    _engine: Engine = PrivateAttr()
 
-    def __init__(self, max_timeout=60, acc=0):
-        try:
-            conection = mysql.connector.connect(
-                host="mysqldb",
-                user="root",
-                password="secret",
-                connect_timeout=100
-            )
-            self.conection = conection
-        except mysql.connector.errors.DatabaseError as error:
-            if acc > max_timeout:
-                raise error
+    def __init__(self, **data) -> None:
+        super().__init__(**data)
+        username = os.environ["MYSQL_USER"]
+        password = os.environ["MYSQL_PASSWORD"]
+        host = os.environ["MYSQL_HOST"]
+        port = os.environ["MYSQL_PORT"]
 
-            time.sleep(10)
-            acc += 10
-            self.__init__(max_timeout, acc)
+        self._engine = create_engine(
+            url=f"mysql+pymysql://{username}:{password}@{host}:{port}",
+            connect_args={"connect_timeout": self.timeout}
+        )
 
-    def create_db(self):
+    def create_db(self) -> None:
         "Create the databse hhrr to stores human resources data."
-        cursor = self.conection.cursor()
-        cursor.execute("DROP DATABASE IF EXISTS hhrr")
-        cursor.execute("CREATE DATABASE hhrr")
-        cursor.close()
+        with self._engine.begin() as conn:
+            conn.execute("DROP DATABASE IF EXISTS hhrr")
+            conn.execute("CREATE DATABASE hhrr")
 
-        return "init database"
-
-    def create_tbl_personas(self):
+    def create_tbl_personas(self) -> None:
         "Create the table personas in the database hhrr."
-        cursor = self.conection.cursor()
-        cursor.execute("DROP TABLE IF EXISTS hhrr.personas")
-        cursor.execute("""
-        CREATE TABLE hhrr.personas(
-          id INT,
-          nombre VARCHAR(100),
-          rut INT,
-          dv CHAR(1),
-          nacimiento DATE NOT NULL,
-          defuncion DATE,
-          PRIMARY KEY (id)
-        )
-        """)
-        cursor.close()
-        return "tbl personas"
+        with self._engine.begin() as conn:
+            conn.execute("DROP TABLE IF EXISTS hhrr.personas")
+            conn.execute("""
+            CREATE TABLE hhrr.personas(
+              id INT,
+              nombre VARCHAR(100),
+              rut INT,
+              dv CHAR(1),
+              nacimiento DATE NOT NULL,
+              defuncion DATE,
+              PRIMARY KEY (id)
+            )
+            """)
 
-    def create_tbl_conyuges(self):
+    def create_tbl_conyuges(self) -> None:
         "Create the table conyuge in the database hhrr."
-        cursor = self.conection.cursor()
-        cursor.execute("DROP TABLE IF EXISTS hhrr.conyuge")
-        cursor.execute("""
-        CREATE TABLE hhrr.conyuges(
-          id INT,
-          id_persona_1 INT,
-          id_persona_2 INT,
-          celebracion DATE NOT NULL,
-          PRIMARY KEY (id),
-          FOREIGN KEY (id_persona_1) REFERENCES hhrr.personas(id),
-          FOREIGN KEY (id_persona_2) REFERENCES hhrr.personas(id)
-        )
-        """)
-        cursor.close()
-        return "tbl conyuges"
+        with self._engine.begin() as conn:
+            conn.execute("DROP TABLE IF EXISTS hhrr.conyuge")
+            conn.execute("""
+            CREATE TABLE hhrr.conyuges(
+              id INT,
+              id_persona_1 INT,
+              id_persona_2 INT,
+              celebracion DATE NOT NULL,
+              PRIMARY KEY (id),
+              FOREIGN KEY (id_persona_1) REFERENCES hhrr.personas(id),
+              FOREIGN KEY (id_persona_2) REFERENCES hhrr.personas(id)
+            )
+            """)
 
-    def create_tbl_hijos(self):
+    def create_tbl_hijos(self) -> None:
         "Create the table hijos in the database hhrr."
-        cursor = self.conection.cursor()
-        cursor.execute("DROP TABLE IF EXISTS hhrr.hijos")
-        cursor.execute("""
-        CREATE TABLE hhrr.hijos(
-          id INT,
-          id_padre INT,
-          id_hijo INT,
-          PRIMARY KEY (id),
-          FOREIGN KEY (id_padre) REFERENCES hhrr.personas(id),
-          FOREIGN KEY (id_hijo) REFERENCES hhrr.personas(id)
-        )
-        """)
-        cursor.close()
-        return "tbl hijos"
+        with self._engine.begin() as conn:
+            conn.execute("DROP TABLE IF EXISTS hhrr.hijos")
+            conn.execute("""
+            CREATE TABLE hhrr.hijos(
+              id INT,
+              id_padre INT,
+              id_hijo INT,
+              PRIMARY KEY (id),
+              FOREIGN KEY (id_padre) REFERENCES hhrr.personas(id),
+              FOREIGN KEY (id_hijo) REFERENCES hhrr.personas(id)
+            )
+            """)
 
-    def truncate_db(self, sample):
+    def truncate_db(self, sample: HumanResourcesGenerator) -> None:
         """
         Truncate hhrr tables with new data.
 
@@ -102,35 +93,35 @@ class HumanResources:
         sample : HumanResourcesGenerator
             Object with data for all tables in hhrr.
         """
+        with self._engine.begin() as conn:
+            conn.execute("DELETE FROM hhrr.conyuges")
+            conn.execute("DELETE FROM hhrr.hijos")
+            conn.execute("DELETE FROM hhrr.personas")
 
-        cursor = self.conection.cursor()
-        cursor.execute("DELETE FROM hhrr.conyuges")
-        cursor.execute("DELETE FROM hhrr.hijos")
-        cursor.execute("DELETE FROM hhrr.personas")
+            conn.execute(
+                text("""
+                  INSERT INTO hhrr.personas (id, nombre, rut, dv,
+                    nacimiento, defuncion)
+                  VALUES (:id, :nombre, :rut, :dv, :nacimiento, :defuncion)
+                  """),
+                sample.personas)
 
-        cursor.executemany("""
-        INSERT INTO hhrr.personas (id, nombre, rut, dv, nacimiento, defuncion)
-        VALUES (
-        %(id)s, %(nombre)s, %(rut)s, %(dv)s, %(nacimiento)s, %(defuncion)s)
-        """, sample.personas)
+            conn.execute(
+                text("""
+                  INSERT INTO hhrr.conyuges (id, id_persona_1, id_persona_2,
+                    celebracion)
+                  VALUES (:id, :id_persona_1, :id_persona_2, :celebracion)
+                  """),
+                sample.conyuges)
 
-        cursor.executemany("""
-        INSERT INTO hhrr.conyuges (id, id_persona_1, id_persona_2, celebracion)
-        VALUES (%(id)s, %(id_persona_1)s, %(id_persona_2)s, %(celebracion)s)
-        """, sample.conyuges)
+            conn.execute(
+                text("""
+                  INSERT INTO hhrr.hijos (id, id_padre, id_hijo)
+                  VALUES (:id, :id_padre, :id_hijo)
+                  """),
+                sample.hijos)
 
-        cursor.executemany("""
-        INSERT INTO hhrr.hijos (id, id_padre, id_hijo)
-        VALUES (%(id)s, %(id_padre)s, %(id_hijo)s)
-        """, sample.hijos)
-
-        self.conection.commit()
-
-        cursor.close()
-
-        return "db populated"
-
-    def create_populated_db(self, sample):
+    def create_populated_db(self, sample: HumanResourcesGenerator) -> None:
         """
         Create the hhrr database and populate it with sample.
 
@@ -144,9 +135,8 @@ class HumanResources:
         self.create_tbl_conyuges()
         self.create_tbl_hijos()
         self.truncate_db(sample)
-        return "populated db created"
 
-    def get_avg_children_per_marriage(self):
+    def get_avg_children_per_marriage(self) -> float:
         """
         Get the average number of children per marriage.
 
@@ -155,54 +145,52 @@ class HumanResources:
         result : float
             Average number of children per marriage.
         """
-        cursor = self.conection.cursor()
-        cursor.execute("""
-        WITH
-          first_parent AS (
+        with self._engine.connect() as conn:
+            result = conn.execute("""
+            WITH
+              first_parent AS (
+                SELECT
+                  c.id,
+                  c.id_persona_1,
+                  c.id_persona_2,
+                  h.id_hijo
+                FROM hhrr.conyuges AS c
+                INNER JOIN hhrr.hijos AS h
+                ON c.id_persona_1 = h.id_padre),
+              second_parent AS (
+                SELECT
+                  c.id,
+                  c.id_persona_1,
+                  c.id_persona_2,
+                  h.id_hijo
+                FROM hhrr.conyuges AS c
+                INNER JOIN hhrr.hijos AS h
+                ON c.id_persona_2 = h.id_padre),
+              marriages_and_children AS (
+                SELECT
+                  fp.id,
+                  fp.id_hijo
+                FROM first_parent AS fp
+                INNER JOIN second_parent AS sp
+                ON fp.id_persona_1 = sp.id_persona_1
+                  AND fp.id_persona_2 = sp.id_persona_2
+                  AND fp.id_hijo = sp.id_hijo),
+              count_children_by_marriage AS (
+                SELECT
+                  id,
+                  count(*) AS num_children
+                FROM marriages_and_children
+                GROUP BY id
+              )
             SELECT
-              c.id,
-              c.id_persona_1,
-              c.id_persona_2,
-              h.id_hijo
+              AVG(IFNULL(chbm.num_children, 0)) avg_per_marriage
             FROM hhrr.conyuges AS c
-            INNER JOIN hhrr.hijos AS h
-            ON c.id_persona_1 = h.id_padre),
-          second_parent AS (
-            SELECT
-              c.id,
-              c.id_persona_1,
-              c.id_persona_2,
-              h.id_hijo
-            FROM hhrr.conyuges AS c
-            INNER JOIN hhrr.hijos AS h
-            ON c.id_persona_2 = h.id_padre),
-          marriages_and_children AS (
-            SELECT
-              fp.id,
-              fp.id_hijo
-            FROM first_parent AS fp
-            INNER JOIN second_parent AS sp
-            ON fp.id_persona_1 = sp.id_persona_1
-              AND fp.id_persona_2 = sp.id_persona_2
-              AND fp.id_hijo = sp.id_hijo),
-          count_children_by_marriage AS (
-            SELECT
-              id,
-              count(*) AS num_children
-            FROM marriages_and_children
-            GROUP BY id
-          )
-        SELECT
-          AVG(IFNULL(chbm.num_children, 0)) avg_per_marriage
-        FROM hhrr.conyuges AS c
-        LEFT JOIN count_children_by_marriage AS chbm
-        ON c.id = chbm.id
-        """)
-        result = float(cursor.fetchall()[0][0])
-        cursor.close()
-        return result
+            LEFT JOIN count_children_by_marriage AS chbm
+            ON c.id = chbm.id
+            """)
+        return float(result.all()[0][0])
 
-    def get_person_with_max_number_grandchildren(self):
+    def get_person_with_max_number_grandchildren(self) -> dict:
         """
         Get the person with the maximum number of grandchildren.
 
@@ -212,34 +200,34 @@ class HumanResources:
             Dictionary with the data of the person with the maximum number of
             grandchildren.
         """
-        cursor = self.conection.cursor()
-        cursor.execute("""
-        WITH
-          count_grandchildren_by_person AS (
-            SELECT
-              grandparents.id_padre,
-              count(*) AS num_grandchildren
-            FROM hhrr.hijos AS grandparents
-            INNER JOIN hhrr.hijos AS parents
-            ON grandparents.id_hijo = parents.id_padre
-            GROUP BY grandparents.id_padre),
-          person_with_max_number_grandchildren AS (
-            SELECT
-                id_padre
-            FROM count_grandchildren_by_person
-            WHERE num_grandchildren = (
-              SELECT
-                MAX(num_grandchildren)
-              FROM count_grandchildren_by_person)
-            LIMIT 1)
-        SELECT *
-        FROM hhrr.personas
-        WHERE id = (
-          SELECT id_padre
-          FROM person_with_max_number_grandchildren)
-        """)
-        result = dict(zip(
-            ("id", "nombre", "rut", "dv", "nacimiento", "defuncion"),
-            cursor.fetchall()[0]))
-        cursor.close()
+
+        with self._engine.connect() as conn:
+            result = conn.execute("""
+            WITH
+              count_grandchildren_by_person AS (
+                SELECT
+                  grandparents.id_padre,
+                  count(*) AS num_grandchildren
+                FROM hhrr.hijos AS grandparents
+                INNER JOIN hhrr.hijos AS parents
+                ON grandparents.id_hijo = parents.id_padre
+                GROUP BY grandparents.id_padre),
+              person_with_max_number_grandchildren AS (
+                SELECT
+                    id_padre
+                FROM count_grandchildren_by_person
+                WHERE num_grandchildren = (
+                  SELECT
+                    MAX(num_grandchildren)
+                  FROM count_grandchildren_by_person)
+                LIMIT 1)
+            SELECT *
+            FROM hhrr.personas
+            WHERE id = (
+              SELECT id_padre
+              FROM person_with_max_number_grandchildren)
+            """)
+            result = dict(zip(
+                ("id", "nombre", "rut", "dv", "nacimiento", "defuncion"),
+                result.all()[0]))
         return result
--- cencosud/question_1/random_generator.py
+++ cencosud/question_1/random_generator.py
@@ -1,32 +1,34 @@
 import string
 import numpy as np
 from datetime import datetime, timedelta
+from typing import List, Dict, Any, Union
+from pydantic import BaseModel, NonNegativeInt
 
 
-class HumanResourcesGenerator:
+class HumanResourcesGenerator(BaseModel):
     """
     Class useful to generate random data to populate a database with the
     tables personas (people), conyuges (marriages) and hijos (children).
 
     Parameters
     ----------
-    n_personas : int
+    n_personas : int, default=1000
         Number of people to sample.
-    n_conyuges : int
+    n_conyuges : int, default=50
         Number of marriages to sample.
-    n_hijos : int
+    n_hijos : int, default=120
         Number of children to sample.
-    min_date : datetime
+    min_date : datetime, default=datetime(1900, 1, 1)
         Minimum random date.
-    max_date : datetime
+    max_date : datetime, default=datetime(2022, 1, 1)
         Maximum random date.
-    seed : int
+    seed : int, default=0
         Random seed.
-    personas: list of dict, default=[]
+    personas : list of dict, default=[]
         A list of random people.
-    conyuges: list of dict, default=[]
+    conyuges : list of dict, default=[]
         A list of random marriages.
-    hijos: list of dict, default=[]
+    hijos : list of dict, default=[]
         A list of random children.
 
     Examples
@@ -34,35 +36,26 @@ class HumanResourcesGenerator:
     >>> hrg = HumanResouresGenerator()
     >>> hrg.sample()
     >>> hrg.personas
-    [{"id": 0, "nombre": "pvadd", "nacimiento": "1952-05-17",
-      "defuncion": "2006-10-22"}, ...]
+    [{"id": 0, "nombre": "pvadd", "nacimiento": datetime(1952, 5, 17),
+      "defuncion": datetime(2006, 10, 22)}, ...]
     >>> hrg.conyuges
     [{"id": 0, "id_persona_1": 10, "id_persona_2": 167,
-      "celebracion": "1936-04-04"}, ...]
+      "celebracion": datetime(1936, 04, 04)}, ...]
     >>> hrg.hijos
     [{"id": 0, "id_padre": 262, "id_hijo": 328},
      {"id": 1, "id_padre": 889, "id_hijo": 328}, ...]
     """
-    def __init__(
-        self,
-        n_personas=1000,
-        n_conyuges=50,
-        n_hijos=120,
-        min_date=datetime(1900, 1, 1),
-        max_date=datetime(2022, 1, 1),
-        seed=0
-    ):
-        self.n_personas = n_personas
-        self.n_conyuges = n_conyuges
-        self.n_hijos = n_hijos
-        self.min_date = min_date
-        self.max_date = max_date
-        self.seed = seed
-        self.personas = []
-        self.conyuges = []
-        self.hijos = []
+    n_personas: NonNegativeInt = 1000
+    n_conyuges: NonNegativeInt = 50
+    n_hijos: NonNegativeInt = 120
+    min_date: datetime = datetime(1900, 1, 1)
+    max_date: datetime = datetime(2022, 1, 1)
+    seed: NonNegativeInt = 0
+    personas: List = []
+    conyuges: List = []
+    hijos: List = []
 
-    def _sample_date(self, min_date, max_date):
+    def _sample_date(self, min_date: datetime, max_date: datetime) -> datetime:
         """
         Sample random date between min_date and max_date.
 
@@ -75,8 +68,8 @@ class HumanResourcesGenerator:
 
         Returns
         -------
-        random_date : str,
-            Random date in format "yyyy-mm-dd".
+        random_date : datetime,
+            Random date.
         """
         time_delta = max_date - min_date
         days = np.random.choice(range(0, time_delta.days + 1))
@@ -85,11 +78,14 @@ class HumanResourcesGenerator:
 
         random_delta = timedelta(int(days), int(seconds), int(microseconds))
         random_date = min_date + random_delta
-        random_date = str(random_date.date())
 
         return random_date
 
-    def _sample_death_date(self, str_birth_date, max_date):
+    def _sample_death_date(
+        self,
+        birth_date: datetime,
+        max_date: datetime
+    ) -> Union[datetime, None]:
         """
         Sample random death date.
 
@@ -98,28 +94,29 @@ class HumanResourcesGenerator:
 
         Parameters
         ----------
-        str_brith_date : str
-          birth date in format "yyyy-mm-dd".
+        brith_date : datetime
+          birth date.
         max_date : datetime
           The random date is set to None if is greather than max_date.
 
         Returns
         -------
-        death_date : str or None
-            A random date in format "yyyy-mm-dd" if is not greather than
-            max_date else None.
+        death_date : datetime or None
+            A random date if is not greather than max_date else None.
         """
-        birth_date = datetime.strptime(str_birth_date, "%Y-%m-%d")
         death_date = birth_date + timedelta(int(np.random.normal(80, 10)*365))
 
         if max_date < death_date:
             death_date = None
-        else:
-            death_date = str(death_date.date())
 
         return death_date
 
-    def _sample_celebration_date(self, persona_1, persona_2, max_date):
+    def _sample_celebration_date(
+        self,
+        persona_1: Dict[str, Any],
+        persona_2: Dict[str, Any],
+        max_date: datetime
+    ) -> Union[datetime, None]:
         """
         Samples random marriage celebration date.
 
@@ -134,24 +131,24 @@ class HumanResourcesGenerator:
 
         Returns
         -------
-        celebration_date : str or None
-           Random marraige celebration date in format "yyyy-mm-dd".
-           The date is generated from the sample of dates where both are 18
-           older, both are alive and before max_date, otherwise None.
+        celebration_date : datetime or None
+           Random marraige celebration date. The date is generated from the
+           sample of dates where both are 18 older, both are alive and before
+           max_date, otherwise None.
         """
-        start_date_1 = datetime.strptime(persona_1["nacimiento"], "%Y-%m-%d")
-        start_date_2 = datetime.strptime(persona_2["nacimiento"], "%Y-%m-%d")
+        start_date_1 = persona_1["nacimiento"]
+        start_date_2 = persona_2["nacimiento"]
         start_date = max(start_date_1, start_date_2) + timedelta(18*365)
 
         if not persona_1["defuncion"] and not persona_2["defuncion"]:
             end_date = max_date
         elif persona_1["defuncion"] and not persona_2["defuncion"]:
-            end_date = datetime.strptime(persona_1["defuncion"], "%Y-%m-%d")
+            end_date = persona_1["defuncion"]
         elif not persona_1["defuncion"] and persona_2["defuncion"]:
-            end_date = datetime.strptime(persona_2["defuncion"], "%Y-%m-%d")
+            end_date = persona_2["defuncion"]
         else:
-            end_date_1 = datetime.strptime(persona_1["defuncion"], "%Y-%m-%d")
-            end_date_2 = datetime.strptime(persona_2["defuncion"], "%Y-%m-%d")
+            end_date_1 = persona_1["defuncion"]
+            end_date_2 = persona_2["defuncion"]
             end_date = min(end_date_1, end_date_2)
 
         if start_date < end_date:
@@ -161,7 +158,7 @@ class HumanResourcesGenerator:
 
         return celebration_date
 
-    def _sample_name(self):
+    def _sample_name(self) -> str:
         """
         Samples random name of lenth within [2, 100].
         """
@@ -170,7 +167,7 @@ class HumanResourcesGenerator:
         name = "".join(np.random.choice(tokens, lenght))
         return name
 
-    def _sample_personas(self):
+    def _sample_personas(self) -> None:
         """
         Fill .personas atribute with a sample of random people.
 
@@ -179,14 +176,8 @@ class HumanResourcesGenerator:
             - nombre: str, string of length [2, 100] with chars in a-z + " ".
             - rut: int, integer number between [0, 2**31].
             - dv: str, a char in "123456789k".
-            - nacimiento: str, a date in format "yyyy-mm-dd".
-            - defuncion: str or None, a date in format "yyyy-mm-dd".
-
-        Returns
-        -------
-        self : object
-          Replace self.personas with a list of random dicts with the following
-          keys [id, nombre, rut, dv, nacimiento, defuncion].
+            - nacimiento: datetime.
+            - defuncion: datetime or None.
         """
         data = []
         for i in range(self.n_personas):
@@ -202,9 +193,8 @@ class HumanResourcesGenerator:
 
             data.append(row)
         self.personas = data
-        return self
 
-    def _sample_conyuges(self):
+    def _sample_conyuges(self) -> None:
         """
         Fill .conyuges atribute with a sample of random marriages.
 
@@ -212,13 +202,7 @@ class HumanResourcesGenerator:
             - id: int, integer number between [0, self.n_conyuges).
             - id_persona_1: int, integer number between [0, self.n_personas).
             - id_persona_2: int, integer number between [0, self.n_personas).
-            - celebracion: str, a date in format "yyyy-mm-dd".
-
-        Returns
-        -------
-        self : object
-          Replace self.conyuges with a list of random dicts with the following
-          keys [id, id_persona_1, id_persona_2, celebracion].
+            - celebracion: datetime.
         """
         data = []
         i = 0
@@ -239,7 +223,6 @@ class HumanResourcesGenerator:
                 i += 1
 
         self.conyuges = data
-        return self
 
     def _sample_hijos(self):
         """
@@ -249,14 +232,7 @@ class HumanResourcesGenerator:
             - id: int, integer number between [0, self.n_hijos).
             - id_padre: int, integer number between [0, self.n_personas).
             - id_hijo: int, integer number between [0, self.n_personas).
-
-        Returns
-        -------
-        self : object
-          Replace self.hijos with a list of random dicts with the following
-          keys [id, id_padre, id_hijo].
         """
-
         i = 0
         data = []
         while i < 2*self.n_hijos:
@@ -274,18 +250,12 @@ class HumanResourcesGenerator:
                 i += 2
 
         self.hijos = data
-        return self
 
-    def sample(self):
+    def sample(self) -> None:
         """
         Fill atributes .personas, .conyuges and .hijos with random data.
-
-        Returns
-        -------
-        self : object
         """
         np.random.seed(self.seed)
         self._sample_personas()
         self._sample_conyuges()
         self._sample_hijos()
-        return self
